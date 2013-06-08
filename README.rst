a5e69453de945969056b295c2f09c5251e1d4638
mwtoews <mwtoews@gmail.com>
2013-06-08 16:55:07 +1200

    Reorganise files into module structure

--- README.rst
+++ README.rst
@@ -1,8 +1,9 @@
 ====
 TTim
 ====
+
 :Author: Mark Bakker
-:Website: http://code.google.com/p/ttim/
+:Website: https://code.google.com/p/ttim/
 
 TTim is a multi-layer transient analytic element solver for modeling
 groundwater flow.
@@ -17,7 +18,30 @@ TTim depends on:
  - scipy
  - matplotlib
 
-Build
------
+Build extensions
+----------------
 
-To build TTim, a Fortran compiler is requird.
+A Fortran 90/95 compiler is requird by f2py to build the Python extensions.
+
+Run the command::
+
+    $ python setup.py build_ext
+
+Or on Windows, specify a compiler, e.g. MinGW::
+
+    $ python.exe setup.py build_ext --fcompiler=gnu95 --compiler=mingw32
+
+Test
+----
+
+Build the extensions in-place, then test::
+
+    $ python setup build_ext --inplace
+    $ python setup test
+
+Install
+-------
+
+Run the command (as root or with ``sudo``)::
+
+    $ python setup install
--- setup.py
+++ setup.py
@@ -1,22 +1,61 @@
 #!/usr/bin/env python
 
+import os
+import sys
+from glob import glob
 from textwrap import dedent
-from numpy.distutils.core import setup, Extension
+from unittest import TextTestRunner, TestLoader
+from numpy.distutils.core import Extension, Command, setup
+
+
+class test(Command):
+    """run unit tests after in-place build"""
+    description = __doc__
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            import ttim
+        except ImportError:
+            self.run_command('build_ext')
+        from ttim.tests import ttim_testsuite
+        tests = TestLoader().loadTestsFromModule(ttim_testsuite)
+        t = TextTestRunner(verbosity=2)
+        t.run(tests)
 
 setup(
-    name='ttim',
-    version='0.22',
-    description=dedent('''\
+    name = 'TTim',
+    version = '0.22',
+    description = dedent('''\
         TTim is a multi-layer transient analytic element solver for modeling
         groundwater flow
     '''),
-    author='Mark Bakker',
-    author_email='mark.bakker@tudelft.nl',
-    url='https://code.google.com/p/ttim/',
-    license='MIT License',
-    py_modules=['ttim'],
-    ext_modules=[
-        Extension('bessel', ['bessel.f95']),
-        Extension('invlap', ['invlap.f90']),
-    ]
+    author = 'Mark Bakker',
+    author_email = 'mark.bakker@tudelft.nl',
+    url = 'https://code.google.com/p/ttim/',
+    license = 'MIT License',
+    requires = ['numpy', 'scipy'],
+    packages = [
+        'ttim',
+        'ttim.tests',
+        'ttim.examples',
+    ],
+    package_data = {
+        'ttim': [
+            'examples/*.dat',
+            'examples/*.fth',
+    ]},
+    # Fortran extensions are built with NumPy's f2py
+    ext_modules = [
+        Extension('ttim.bessel', ['ttim/bessel.f95']),
+        Extension('ttim.invlap', ['ttim/invlap.f90']),
+    ],
+    # python setup.py test
+    cmdclass = {'test': test},
 )
